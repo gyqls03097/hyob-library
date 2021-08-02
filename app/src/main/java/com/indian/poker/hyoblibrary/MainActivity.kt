dc6c25953e2aa705c82dc9e0dd0dd596406bd98b
hyobin.roh <gyqls03097@gmail.com>
2021-08-02 22:00:17 +0900

    create project

---
+++ app/src/main/java/com/indian/poker/hyoblibrary/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.indian.poker.hyoblibrary
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import common.library.core.HyobCore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.hello)
+        textView.text = HyobCore.core()
+    }
+}
---
+++ common/src/main/java/common/library/core/HyobCore.kt
@@ -0,0 +1,8 @@
+package common.library.core
+
+object HyobCore {
+
+    fun core(): String {
+        return "this is hyob core"
+    }
+}
